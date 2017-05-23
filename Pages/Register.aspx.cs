9ba7316ae42a901e855f5217f21fe9a05500424e
Ron soffer <ronso008@Gmail.com>
2017-05-24 01:37:06 +0300

    asd

--- Pages/Register.aspx.cs
+++ Pages/Register.aspx.cs
@@ -22,43 +22,46 @@ public partial class Pages_Register : System.Web.UI.Page
 
     protected void submit_Click(object sender, EventArgs e)
     {
-        UserDetails user2 = new UserDetails();
-        UserService userService = new UserService();
-
-
-        string Name = name.Text;
-        string Email = email.Text;
-        string Username = username.Text;
-        DateTime Date = Convert.ToDateTime(birthday.Text);
-        string Password = password.Text;
-        string Confirm = confirm.Text;
-
-        user2 = userService.GetUser(Username);
-        if (Password != Confirm || user2 != null)
+        if (Page.IsValid)
         {
-            userMsg.Attributes.Add("class", "alert alert-danger");
-            passwordError.Attributes.Add("class", "input-group has-error");
-            confirmError.Attributes.Add("class", "input-group has-error");
-            if (Password != Confirm)
-                msg = "Passwords not Match! ";
-            if(user2 != null)
-                msg = "Username already Exist!";
-        }
-        else
-        {
-            UserDetails user = new UserDetails();
-            userMsg.Attributes.Add("class", "alert alert-success");
-            msg = "Success!";
-            user.Name = Name;
-            user.Email = Email;
-            user.Username = Username;
-            user.Birthday = Date;
-            user.Password = Password;
+            UserDetails user2 = new UserDetails();
+            UserService userService = new UserService();
 
-            userService.InserUser(user);
 
-            Session["User"] = Name;
-            Session["Username"] = Username;
+            string Name = name.Text;
+            string Email = email.Text;
+            string Username = username.Text;
+            DateTime Date = Convert.ToDateTime(birthday.Text);
+            string Password = password.Text;
+            string Confirm = confirm.Text;
+
+            user2 = userService.GetUser(Username);
+            if (Password != Confirm || user2 != null)
+            {
+                userMsg.Attributes.Add("class", "alert alert-danger");
+                passwordError.Attributes.Add("class", "input-group has-error");
+                confirmError.Attributes.Add("class", "input-group has-error");
+                if (Password != Confirm)
+                    msg = "Passwords not Match! ";
+                if (user2 != null)
+                    msg = "Username already Exist!";
+            }
+            else
+            {
+                UserDetails user = new UserDetails();
+                userMsg.Attributes.Add("class", "alert alert-success");
+                msg = "Success!";
+                user.Name = Name;
+                user.Email = Email;
+                user.Username = Username;
+                user.Birthday = Date;
+                user.Password = Password;
+
+                userService.InserUser(user);
+
+                Session["User"] = Name;
+                Session["Username"] = Username;
+            }
         }
     }
 }
